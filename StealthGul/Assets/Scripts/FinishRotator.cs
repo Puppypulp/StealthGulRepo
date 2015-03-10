6044804ba0777d05aaf02863ecc21925eb8c36fa
Puppypulp <puppypulp@gmail.com>
2015-03-09 23:17:36 -0700

    Spooky commit

---
+++ StealthGul/Assets/Scripts/FinishRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishRotator : MonoBehaviour 
+{
+	public float x;
+	public float y;
+	public float z;
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (new Vector3(x, y, z) * speed * Time.deltaTime);
+	}
+}
---
+++ StealthGul/Assets/Scripts/PlayerControllerOld.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerControllerOld : MonoBehaviour //unity needs things serialized to see them in instructor
+{
+	public float speed; ///allows the speed of the character to be fixed
+	//public Text countText;
+	//public Text winText;
+	private int count;
+
+	void Start ()
+	{
+		count = 0;
+		/*SetCountText(); //DONT enter the "void" part, stupid
+		winText.text = ""; */
+	}
+
+	void FixedUpdate () ///FixedUpdate happens before every frame
+	{
+		///The input for player control
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		///Simplest way to move the character
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+
+		///makes it so the seagul doesn't spin
+		GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			//SetCountText();
+		}
+	}
+	/*Creating a function to be called. Will display the count
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if(count >=8)
+		{
+			winText.text = "You Won, Nigga!";
+		}
+	} */
+}
